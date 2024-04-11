2e05b27361fbcdf11d93dfde37ee3dd57f28c0c1
Govind <113257576+govindKulk@users.noreply.github.com>
2024-04-12 01:48:29 +0530

    Update ChatHeader.jsx

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -10,14 +10,14 @@ const ChatHeader = ({
         <div className='flex flex-row gap-2 justify-between bg-[#eff3fd] p-4 text-blue-500 rounded-t-[2rem]'>
             <div className='flex flex-row gap-2'>
                 <div className='rounded-full relative'>
-                <img src={'../../public/assets/chatbot.png'} alt="Chatbot" className='rounded-full' />
+                <img src={'/public/assets/chatbot.png'} alt="Chatbot" className='rounded-full' />
                 <span className={`w-2 h-2 block ${receipientStatus == 'online' ? 'bg-green-400' : 'bg-red-500'} rounded-[100%] absolute right-1 bottom-2 `}></span>
                 </div>
                 <div className='flex flex-col'>
 
                     <div className='flex flex-row gap-2 items-center'>
                         <p className=' text-2xl font-bold'>Timpu </p>
-                        <img src="../../public/assets/goldbadge.png" alt="gold badge" className='' />
+                        <img src="/public/assets/goldbadge.png" alt="gold badge" className='' />
                     </div>
 
 
